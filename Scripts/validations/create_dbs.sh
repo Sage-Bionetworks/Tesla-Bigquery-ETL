37ab9a4339377c9b63d384d188b0d89525caee05
andrewelamb <andrewelamb@gmail.com>
2018-12-04 08:08:12 -0800

    various

--- Scripts/validations/create_dbs.sh
+++ Scripts/validations/create_dbs.sh
@@ -1,13 +1,3 @@
-bq mk --table test.TCR_Binding_by_Flow_I ../../Data/table_json/TCR_Binding_by_Flow_I.json
+bq mk --table Version_2.Validated_bindings ../../Data/table_json/Validated_bindings.json
 
-bq mk --table test.TCR_Binding_by_Flow_II ../../Data/table_json/TCR_Binding_by_Flow_II.json
-
-bq mk --table test.TCR_Binding_by_Microfluidics ../../Data/table_json/TCR_Binding_by_Microfluidics.json
-
-bq mk --table test.T_Cell_Reactivity_Screen ../../Data/table_json/T_Cell_Reactivity_Screen.json
-
-bq mk --table test.MHC_Binding_Assay ../../Data/table_json/MHC_Binding_Assay.json
-
-bq mk --table test.MS_Peptide_IDs ../../Data/table_json/MS_Peptide_IDs.json
-
-bq mk --table test.Validations ../../Data/table_json/Validations.json
+bq mk --table Version_2.Validated_epitopes ../../Data/table_json/Validated_epitopes.json
