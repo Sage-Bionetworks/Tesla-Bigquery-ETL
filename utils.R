3457cb052fd3c45fbe76a9b47f89bf6bde5e1585
andrewelamb <andrewelamb@gmail.com>
2019-03-05 07:26:24 -0800

    removed not needed functions

--- utils.R
+++ utils.R
@@ -50,7 +50,6 @@ get_file_df_from_synapse_dir_id <- function(syn_id){
 
 require(magrittr)
 require(tibble)
-require(plyr)
 require(purrr)
 
 transpose_df <- function(df, id_column, new_col){
@@ -72,66 +71,5 @@ matrix_to_df <- function(matrix, new_col){
         data.frame() %>% 
         tibble::rownames_to_column(new_col) %>% 
         tibble::as_tibble()
-}
-
-
-aggregate_matrix <- function(
-    matrix, grouping_df, group_by, group_accross, 
-    aggregate_fun = mean,
-    split_by_cols = T,
-    apply_margin = 1,
-    combine_fun = cbind,
-    parallel = T){
-    
-    group_list <- create_group_list(grouping_df, group_by, group_accross)
-    matrix %>%
-        split_matrix(group_list, split_by_cols, parallel) %>% 
-        plyr::llply(apply, apply_margin, aggregate_fun, .parallel = parallel) %>% 
-        purrr::invoke(combine_fun, .) 
-}
-
-split_matrix <- function(matrix, lst, by_cols = T, parallel = T){
-    fun <- ifelse(T, 
-                  function(names) matrix[,names, drop = F], 
-                  function(names) matrix[names, drop = F])
-    plyr::llply(lst, fun, .parallel = parallel)
-}
-
-
-create_group_list <- function(df, group_by, group_accross){
-    df %>%
-        magrittr::extract2(group_by) %>% 
-        split(df, .) %>%
-        purrr::map(extract2, group_accross)
-}
-
-
-# misc ------------------------------------------------------------------------
-
-require(magrittr)
-require(preprocessCore)
-
-get_summary_by_matrix_cols <- function(columns, matrix, fn){
-    m <- matrix[,columns]
-    if(length(columns) == 1) return(m)
-    apply(m, 1, fn)
-}
-
-calculate_cpm <- function(counts){
-    1000000 * (counts/sum(counts))
-}
-
-zscore_matrix <- function(matrix){
-    matrix %>% 
-        apply(1, scale) %>% 
-        t %>% 
-        magrittr::set_colnames(colnames(matrix))
-}
-
-quantile_normalize_matrix <- function(matrix){
-    matrix %>% 
-        preprocessCore::normalize.quantiles() %>% 
-        magrittr::set_colnames(colnames(matrix)) %>% 
-        magrittr::set_rownames(rownames(matrix))
 }
 
