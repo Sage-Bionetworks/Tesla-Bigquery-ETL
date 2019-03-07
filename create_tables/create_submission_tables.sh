2da3a519bb12a51d5816cbcdc82dabbe0e0dee5e
andrewelamb <andrewelamb@gmail.com>
2019-03-07 13:25:21 -0800

    various

--- create_tables/create_submission_tables.sh
+++ create_tables/create_submission_tables.sh
@@ -4,3 +4,4 @@ bq mk --table Version_3.Steps json/Steps.json
 bq mk --table Version_3.Variants json/Variants.json
 bq mk --table Version_3.Parameters json/Parameters.json
 bq mk --table Version_3.Predictions json/Predictions.json
+bq mk --table Version_3.Bad_Predictions json/Predictions.json
