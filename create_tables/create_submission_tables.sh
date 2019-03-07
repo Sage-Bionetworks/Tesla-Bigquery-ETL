3152585ab9cb0ae3fbc1e6eac5f25219dcea8773
andrewelamb <andrewelamb@gmail.com>
2019-03-07 11:30:58 -0800

    first commits

---
+++ create_tables/create_submission_tables.sh
@@ -0,0 +1,6 @@
+bq mk --table Version_3.Protein_Positions json/Protein_Positions.json
+bq mk --table Version_3.Prediction_Variants json/Prediction_Variants.json
+bq mk --table Version_3.Steps json/Steps.json
+bq mk --table Version_3.Variants json/Variants.json
+bq mk --table Version_3.Parameters json/Parameters.json
+bq mk --table Version_3.Predictions json/Predictions.json
