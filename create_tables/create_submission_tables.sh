efc80ae3f1051cbf2092cff78ba5abb763103cc6
andrewelamb <andrewelamb@gmail.com>
2019-03-08 10:51:50 -0800

    added team and survey tables

--- create_tables/create_submission_tables.sh
+++ create_tables/create_submission_tables.sh
@@ -6,3 +6,7 @@ bq mk --table Version_3.Steps json/Steps.json
 bq mk --table Version_3.Parameters json/Parameters.json
 bq mk --table Version_3.Variants json/Variants.json
 bq mk --table Version_3.Submissions json/Submissions.json
+
+bq mk --table Version_3.Teams json/Teams.json
+bq mk --table Version_3.Survey_Answers json/Survey_Answers.json
+bq mk --table Version_3.Survey_Questions json/Survey_Questions.json
