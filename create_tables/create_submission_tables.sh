eebc71afe2a782ef99e4402c3e52892cdd9b899d
andrewelamb <andrewelamb@gmail.com>
2019-03-07 17:15:57 -0800

    redid column order

--- create_tables/create_submission_tables.sh
+++ create_tables/create_submission_tables.sh
@@ -1,8 +1,8 @@
 bq mk --table Version_3.Protein_Positions json/Protein_Positions.json
 bq mk --table Version_3.Prediction_Variants json/Prediction_Variants.json
-bq mk --table Version_3.Steps json/Steps.json
-bq mk --table Version_3.Variants json/Variants.json
-bq mk --table Version_3.Parameters json/Parameters.json
 bq mk --table Version_3.Predictions json/Predictions.json
 bq mk --table Version_3.Bad_Predictions json/Predictions.json
+bq mk --table Version_3.Steps json/Steps.json
+bq mk --table Version_3.Parameters json/Parameters.json
+bq mk --table Version_3.Variants json/Variants.json
 bq mk --table Version_3.Submissions json/Submissions.json
