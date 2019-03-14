c6d80a510c954e0993943a009ef69fb9b3c09f1b
andrewelamb <andrewelamb@gmail.com>
2019-03-14 14:28:03 -0700

    first commits

---
+++ upload_data/Submissions/round2/upload_round2_submissions.R
@@ -0,0 +1,76 @@
+# must run ./query_queues.sh to get correct files
+
+library(synapser)
+library(tidyverse)
+library(magrittr)
+library(lubridate)
+library(bigrquery)
+
+source("../../../utils.R")
+
+synLogin()
+
+file_df <- 
+    "select id, name, submissionId, team, patientId from syn18387034 where round = '2'" %>% 
+    synapser::synTableQuery() %>% 
+    as.data.frame() %>% 
+    dplyr::mutate(file_type = str_match(name, "TESLA_[:print:]+$")) %>% 
+    dplyr::select(id, file_type, submissionId, team, patientId) %>% 
+    tidyr::spread(key = "file_type", value = "id") %>% 
+    magrittr::set_colnames(c(
+        "SUBMISSION_ID",
+        "TEAM",
+        "PATIENT_ID",
+        "FASTQ_RANKED_FILE",
+        "VCF_RANKED_FILE",
+        "FASTQ_UNRANKED_FILE",
+        "VCF_UNRANKED_FILE",
+        "VCF_FILE",
+        "YAML_FILE"))
+    
+submission_df <- "../../../round12.csv" %>% 
+    readr::read_csv() %>% 
+    dplyr::filter(round == "2") %>% 
+    dplyr::mutate(createdOn = lubridate::as_datetime(createdOn / 1000)) %>% 
+    dplyr::select(createdOn, objectId) %>% 
+    magrittr::set_colnames(c(
+        "DATETIME", 
+        "SUBMISSION_ID")) %>% 
+    dplyr::mutate(SUBMITTED_LATE = F) %>% 
+    dplyr::mutate(SUBMISSION_ID = as.character(SUBMISSION_ID)) %>% 
+    dplyr::mutate(ROUND = '2')
+
+auprc_df <- "syn18411654" %>% 
+    create_df_from_synapse_id() %>% 
+    dplyr::select(-V1) %>%
+    dplyr::rename(PATIENT_ID = PATIENT) %>% 
+    dplyr::mutate(PATIENT_ID = stringr::str_remove_all(PATIENT_ID, "Patient_"))
+
+combined_df <- 
+    dplyr::inner_join(file_df, submission_df) %>% 
+    dplyr::left_join(auprc_df) %>% 
+    dplyr::as_tibble() %>% 
+    dplyr::mutate(KEY_FILE = NA) %>% 
+    dplyr::mutate(STEP_FILE = NA) %>% 
+    dplyr::mutate(YAML_FILE = NA) %>%
+    dplyr::select(
+        SUBMISSION_ID,
+        PATIENT_ID,
+        TEAM,
+        DATETIME,
+        ROUND,
+        AUPRC,
+        SUBMITTED_LATE,
+        FASTQ_RANKED_FILE,
+        FASTQ_UNRANKED_FILE,
+        VCF_RANKED_FILE,
+        VCF_UNRANKED_FILE,
+        VCF_FILE,
+        YAML_FILE,
+        KEY_FILE,
+        STEP_FILE 
+    )
+
+tbl <- bigrquery::bq_table("neoepitopes", "Version_3", table = "Submissions")
+bigrquery::bq_table_upload(tbl, combined_df, write_disposition = "WRITE_APPEND")
+
---
+++ upload_data/Teams/upload_team_tables.R
@@ -0,0 +1,81 @@
+library(synapser)
+library(tidyverse)
+library(magrittr)
+library(bigrquery)
+
+
+source("../../utils.R")
+
+synLogin()
+
+tesla_team_df <- "SELECT * from syn8220615" %>% 
+    synapser::synTableQuery() %>% 
+    as.data.frame() %>% 
+    tibble::as_tibble() %>% 
+    dplyr::select(alias) %>%
+    dplyr::rename(TEAM = alias) %>% 
+    dplyr::distinct()
+
+survey_df1 <- "syn11300043" %>% 
+    create_df_from_synapse_id() %>% 
+    dplyr::rename(TEAM = V1)
+
+team_df <- tesla_team_df %>% 
+    dplyr::full_join(survey_df1) %>% 
+    dplyr::select(TEAM, Comments) %>% 
+    dplyr::rename(SURVEY_COMMENTS = Comments)
+
+survey_df2 <- survey_df1 %>% 
+    dplyr::select(-Comments) %>% 
+    tidyr::gather(key = "question", value = "ANSWER", - TEAM) %>% 
+    tidyr::drop_na()
+
+survey_df3 <- survey_df2 %>% 
+    dplyr::select(-c(TEAM, ANSWER)) %>% 
+    dplyr::distinct() %>% 
+    dplyr::mutate(QUESTION_NAME = stringr::str_c("Question", 1:n()))
+
+survey_answers_df <- survey_df2 %>% 
+    dplyr::left_join(survey_df3) %>% 
+    dplyr::select(TEAM, QUESTION_NAME, ANSWER) %>% 
+    dplyr::mutate(ANSWER = if_else(ANSWER == "yes", T, F))
+
+survey_questions_df <- survey_df3 %>% 
+    tidyr::separate(question, sep = ": ", into = c(
+        "CATEGORY", "QUESTION_PART_1", "QUESTION_PART_2")) %>% 
+    dplyr::mutate(QUESTION_PART_2 = stringr::str_remove_all(
+        QUESTION_PART_2, "[\\[\\]]")) %>% 
+    dplyr::select(QUESTION_NAME, CATEGORY, QUESTION_PART_1, QUESTION_PART_2)
+
+team_tbl <- bigrquery::bq_table(
+    "neoepitopes", 
+    "Version_3", 
+    table = "Teams")
+
+bigrquery::bq_table_upload(
+    team_tbl, 
+    team_df, 
+    write_disposition = "WRITE_APPEND")
+
+survey_answers_tbl <- bigrquery::bq_table(
+    "neoepitopes", 
+    "Version_3",
+    table = "Survey_Answers")
+
+bigrquery::bq_table_upload(
+    survey_answers_tbl, 
+    survey_answers_df, 
+    write_disposition = "WRITE_APPEND")
+
+survey_questions_tbl <- bigrquery::bq_table(
+    "neoepitopes", 
+    "Version_3",
+    table = "Survey_Questions")
+
+bigrquery::bq_table_upload(
+    survey_questions_tbl, 
+    survey_questions_df, 
+    write_disposition = "WRITE_APPEND")
+
+
+
